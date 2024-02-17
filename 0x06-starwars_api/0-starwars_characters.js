75514b4f488a913e672e4b83a5cc97c2429fe3fe
Mukona21 <mutshinyen@gmail.com>
2024-02-17 17:48:40 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,48 +2,52 @@
 
 const request = require('request');
 
-const args = process.argv.slice(2); // Get movie ID from arguments
+const movieId = process.argv[2];
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+let people = [];
+const names = [];
 
-if (!args.length) {
-  console.error('Please provide a movie ID.');
-  process.exit(1);
-}
+const requestCharacters = async () => {
+  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
+      resolve();
+    }
+  }));
+};
 
-const movieId = args[0];
-
-const url = `https://swapi.dev/api/films/${movieId}`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching movie data:', error);
-    process.exit(1);
+const requestNames = async () => {
+  if (people.length > 0) {
+    for (const p of people) {
+      await new Promise(resolve => request(p, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
+    }
+  } else {
+    console.error('Error: Got no Characters for some reason');
   }
+};
 
-  if (response.statusCode !== 200) {
-    console.error('Invalid movie ID or API error:', response.statusCode);
-    process.exit(1);
+const getCharNames = async () => {
+  await requestCharacters();
+  await requestNames();
+
+  for (const n of names) {
+    if (n === names[names.length - 1]) {
+      process.stdout.write(n);
+    } else {
+      process.stdout.write(n + '\n');
+    }
   }
+};
 
-  const movieData = JSON.parse(body);
-
-  // Get characters in the same order as "characters" list
-  const characters = movieData.characters;
-
-  // Print each character name on a separate line
-  characters.forEach(characterUrl => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error('Error fetching character data:', error);
-        process.exit(1);
-      }
-
-      if (response.statusCode !== 200) {
-        console.error('Invalid character URL or API error:', response.statusCode);
-        process.exit(1);
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  });
-});
+getCharNames();
